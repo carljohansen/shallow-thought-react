10ff437e4f72a7c7b56f5adab7753eea1dbfda91
Carl Johansen <carl@carljohansen.co.uk>
2024-06-06 22:12:07 +1000

    New feature: PGN output

--- src/MoveList.tsx
+++ src/MoveList.tsx
@@ -25,20 +25,42 @@ const MoveList: FC<MoveListProps> = ({ moveList }) => {
         ));
     }
 
+    function showPgn() {
+        let result = "";
+        let moveNumber = 1;
+        for (let i = 0; i < moveList.length; i += 2) {
+            const whiteMove = Chess.GameMove.getNotation(moveList[i]);
+            const blackMove = i < moveList.length - 1
+                ? Chess.GameMove.getNotation(moveList[i + 1])
+                : "";
+            result += `${moveNumber++}. ${moveToPgn(whiteMove)} ${moveToPgn(blackMove)} `;
+        }
+
+        console.log("Game PGN:");
+        console.log(result);
+    }
+
+    function moveToPgn(move: string) {
+        return move.replace("\u00D7", "x");
+    }
+
     return (
-        <div className='movelist-wrapper'>
-            <table>
-                <thead>
-                    <tr>
-                        <td>Move</td>
-                        <td>White</td>
-                        <td>Black</td>
-                    </tr>
-                </thead>
-                <tbody>
-                    {rows}
-                </tbody>
-            </table>
+        <div className='movelist-outer'>
+            <div className='movelist-wrapper'>
+                <table>
+                    <thead>
+                        <tr>
+                            <td>Move</td>
+                            <td>White</td>
+                            <td>Black</td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {rows}
+                    </tbody>
+                </table>
+            </div>
+            <button onClick={showPgn}>PGN</button>
         </div>
     )
 }
